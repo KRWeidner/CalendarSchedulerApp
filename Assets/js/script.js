210cde7044826835a180df6b50cc4267bb98d9bf
KatyW <ktabe13@gmail.com>
2023-06-04 00:33:08 -0500

    adding localstorage logic to save/get events

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,45 +12,29 @@ var calendarEvent = [{
 var today = dayjs();
 var currentHour = today.format('H');
 var amPm = "AM";
+var index = 0;
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-  //hourBlock.addClass('future');//past,future
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // Displays the current date in the header section of the page.
   currentDay.text(today.format('dddd, MMMM D'));
 
-  for (var i = 9; i < 19; i++) {  
-    var addedClass; 
-    if(i===9)
-    {
+  for (var i = 9; i < 19; i++) {
+    var addedClass;
+    if (i === 9) {
       hourBlock.children().eq(0).text(i + amPm);
-      addedClass = compareTime(i,amPm,hourBlock);
+      hourBlock.attr('id', index);
+      addedClass = compareTime(i, hourBlock);
+      getSavedEvent(index);    
     }
-    else
-    {
+    else {
+      index += 1;
       amPm = "PM";
       var timeSlot = hourBlock.clone().appendTo(calendar);
       timeSlot.removeClass(addedClass);
+      timeSlot.attr('id', index);
       if (i > 12) {
         timeSlot.children().eq(0).text(Math.abs([i - 12]) + amPm);
       }
@@ -60,7 +44,8 @@ $(function () {
         }
         timeSlot.children().eq(0).text(i + amPm);
       }
-      compareTime(i,amPm,timeSlot);
+      compareTime(i, timeSlot);
+      getSavedEvent(index);  
     }
   }
 
@@ -69,29 +54,46 @@ $(function () {
       var calEventName = $.trim($('textarea').eq(index).val());
       var scheduledHour = $.trim($(this).parent().closest('div').text());
       calendarEvent.push({ index: index, hour: scheduledHour, event: calEventName });
-      console.log(calendarEvent);
+      var savedEvents = localStorage.getItem('calEvents');
+      if (savedEvents != null) {
+        var savedEventsArray = JSON.parse(savedEvents);
+        savedEventsArray.forEach(event => {
+          calendarEvent.push(event);
+        });
+      }
+      localStorage.setItem('calEvents', JSON.stringify(calendarEvent));
     });
   });
 });
 
-function compareTime(hour, unit,timeSlot)
-{
+function compareTime(hour, timeSlot) {
   var classColoring;
   //if current Hour equals time slot hour than set to present
-  if(currentHour == hour)
-  {
+  if (currentHour == hour) {
     timeSlot.addClass("present");
-    classColoring="present";
+    classColoring = "present";
   }//if current Hour is less than(earlier) than the time slot time add future class
-  else if(currentHour < hour)
-  {
+  else if (currentHour < hour) {
     timeSlot.addClass("future");
-    classColoring="future";
+    classColoring = "future";
   }//if current Hour is greater than(later) than the time slot time add past class
-  else
-  {
+  else {
     timeSlot.addClass("past");
-    classColoring="past";
+    classColoring = "past";
   }
   return classColoring;
+}
+
+function getSavedEvent(indexEvent) {
+  var savedEvents = localStorage.getItem('calEvents');
+  if (savedEvents != null) {
+    var savedEventsArray = JSON.parse(savedEvents);
+    if (savedEventsArray.some(event => event.index == indexEvent)) {
+      var savedTimeSlotEvent = savedEventsArray.find(event => event.index == indexEvent);
+      $('#' + index).children("textarea").text(savedTimeSlotEvent.event);
+    }
+    else {
+      $('#' + index).children("textarea").text("")
+    }
+  }
 }
